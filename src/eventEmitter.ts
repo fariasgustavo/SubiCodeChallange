d83fbe6a004448c6a9368552f66fd6b32d4ebaa1
Gustavo Farias <devgustavofarias@gmail.com>
2023-04-11 12:07:08 +1000

    feat(event emitter): register and emit methods either for create and call a new event

--- src/eventEmitter.ts
+++ src/eventEmitter.ts
@@ -1,5 +1,13 @@
 export class EventEmitter {
+    private events: Record<string, (payload?: Record<string, any>) => void> = {};
+    
     constructor() {}
 
- 
+    public register(eventName: string, eventHandler: (payload?: Record<string, any>) => void): void {
+        this.events[eventName] = eventHandler;
+    }
+
+    public emit(eventName: string, payload?: Record<string, any>): void {
+        this.events[eventName](payload);
+    }
 }
